3984fa7a77fb7f672ed50e69d8c1c1fa264b3842
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-06-04 19:58:13 +0530

    Update add blog UI

---
+++ 07Bloggie-TheDevBlog/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _07Bloggie_TheDevBlog.Pages.Admin.Blogs
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
