22d65c9fa7863b3296083681a4adccf3af230e91
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-06-04 20:28:26 +0530

    add view model

--- 07Bloggie-TheDevBlog/Pages/Admin/Blogs/Add.cshtml.cs
+++ 07Bloggie-TheDevBlog/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,11 +1,19 @@
+using _07Bloggie_TheDevBlog.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _07Bloggie_TheDevBlog.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+           
         }
     }
 }
